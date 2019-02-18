5556e5a7e725fe9f97de4a2f59fa3a47747290b2
mbela-kevinn <mbalevin@gmail.com>
2019-02-18 15:57:40 +0100

    ajout des modifs

--- sprint/s1.js
+++ sprint/s1.js
@@ -1,1 +1,12 @@
-console.log("okacode");
+
+let nombre1;
+let nombre2;
+nombre1 = prompt('Entrez le premier nombre');
+nombre2 = prompt('Entrez le deuxième nombre');
+if(nombre1 < nombre2){
+   console.log(nombre2);
+}else if(nombre1 > nombre2){
+   console.log(nombre1);   
+}else{
+    console.log("les nombres égaux");
+}
--- sprint/s2.js
+++ sprint/s2.js
@@ -1,7 +1,2 @@
-const nombre = Number(prompt("Entrez un nombre :"));
-if (nombre > 18) {
-  console.log("vous etes majeur");
-}
-else {
-  console.log( "vous etes mineur");
-}
+const prenom = prompt("Entrez votre prenom:");
+alert(`Bonjour, ${prenom}`);
