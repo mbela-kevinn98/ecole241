62267aade27c1ffd8357cd5031a52444cbfb80ee
mbela-kevinn <mbalevin@gmail.com>
2019-02-18 14:28:00 +0100

    ajout de s6

--- sprint/s2.js
+++ sprint/s2.js
@@ -1,6 +1,7 @@
-Soit nbre =  5 ;
-si (nbre >  10 ) {
-    console . log ( " Supérieur " );    
-} else {
-    console . log ( " Inférieur " );
+const nombre = Number(prompt("Entrez un nombre :"));
+if (nombre > 18) {
+  console.log("vous etes majeur");
+}
+else {
+  console.log( "vous etes mineur");
 }
