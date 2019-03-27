e76671e4213ced50c61afc57aec959bf4b063c5a
mbela-kevinn <mbalevin@gmail.com>
2019-03-27 13:32:28 +0100

    sprintphp

--- sprintphp/php/act1.php
+++ sprintphp/php/act1.php
@@ -1,28 +1,27 @@
-<!DOCTYPE html > 
-<html> 
-<head> 
-	<title>Le top 10</title> 
-</head>
-<body> 
-	<h2>Le top 10</h2> 
+<!doctype html>
 
+<html>
 
-<? Php
+    <head>
+        <title > Act4 </title>
+        <meta  charset = " UTF-8 " >
+    </head>
 
-$ noms0 = "basket";
-$ noms1 = "ballon";
-$ noms2 = "joueur";
-$ noms3 = "tete";
-$ noms4 = "libreville";
-$ noms5 = "tokyo";
-$ noms6 = "tarzan";
-$ noms7 = "viande";
-$ noms8 = "poulet";
-$ noms9 = "avion";
-$ noms = array ( "basket" , "ballon" , "joueur" , "tete" , "libreville" , "tokyo" ,"tarzan" , "viande" , "poulet" , "avion");
-
-echo "Les prenoms" . $ noms [ 0 ]. "," . $ noms [ 1 ]. "et" . $ noms [ 2 ]. "." ; . $ noms [ 3 ]. "," . $ noms [ 4 ]. " , " . $ noms [ 5 ]. "." . $ noms [ 6 ]. "," . $ noms [ 7 ]. "," . $ noms [ 8 ]. " "  $ noms [ 9 ].;
-?> 
-
-</body> 
+    <body>
+    	<h1>Tableau</h1>
+        <? php
+        $ okacode  =  array (
+            1  =>  array ( ' nom '  =>  ' Kelby ' , ' prenom '  =>  ' Kevinn ' , ' age '  =>  ' 21 ' , ' profil '  =>  ' mbela-kevinn98.github.io/ecole241/profil/ ' ) ,
+            2  =>  array ( ' nom '  =>  ' John ' , ' prenom '  =>  ' joe ' , ' age '  =>  ' 25 ' , ' profil '  => ' .github.io/ecole241/profil/ ' ) ,
+            3  =>  array ( ' nom '  =>  ' Tef ' , ' prenom '  =>  ' hamilton' , ' age '  =>  ' 25 ' , ' profil '  =>  ' .github.io/ecole241/profil/ ' ) ,
+            4  =>  array ( ' nom '  =>  ' Atol ' , ' prenom '  =>  ' axel ' , ' age '  =>  ' 27 ' , ' profil '  =>  ' .github.io/ecole241/profil/ ' ) ,
+            5  =>  array ( ' nom '  =>  ' Tefl ' , ' prenom '  =>  ' Laurion ' , ' age '  =>  ' 24 ' , ' profil '  =>  ' TEFL ' ),
+            6  =>  array ( ' nom '  =>  ' Tchop ' , ' prenom '  =>  ' grace ' , ' age '  =>  ' 28 ' , ' profil '  =>  ' Fapeterie ' ),
+            7  =>  array ( ' nom '  =>  ' dodo ' , ' prenom '  =>  ' ivan ' , ' age '  =>  ' 27 ' , ' profil '  =>  ' .github.io/ecole241/profil/ ' ),
+            8  =>  array ( ' nom '  =>  ' Boulengibindza ' , ' prenom '  =>  ' falana ' , ' age '  =>  ' 19 ' , ' profil '  =>  ' .github.io/ecole241/profil/index. html ' ),
+            9  =>  array ( ' nom '  =>  ' Git ' , ' prenom '  =>  ' vilain petit canard ' , ' age '  =>  ' 22 ' , ' profil '  =>  ' .github.io/ecole241/profil/ ' ) ,
+            10  =>  array ( ' nom '  =>  ' Kog ' , ' prenom '  =>  ' ebanete ' , ' age '  =>  ' 20 ' , ' profil '  => ' darel14.github.io/ecole241/profil/ ' )
+        )
+        ?> 
+    </body>
 </html>
