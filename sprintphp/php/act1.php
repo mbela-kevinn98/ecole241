1bd9bb8184487794b65d0847ff05f2f03caa1c05
mbela-kevinn <mbalevin@gmail.com>
2019-03-27 11:42:04 +0100

    sprint

---
+++ sprintphp/php/act1.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html > 
+<html> 
+<head> 
+	<title>Le top 10</title> 
+</head>
+<body> 
+	<h2>Le top 10</h2> 
+
+
+<? Php
+
+$ noms0 = "basket";
+$ noms1 = "ballon";
+$ noms2 = "joueur";
+$ noms3 = "tete";
+$ noms4 = "libreville";
+$ noms5 = "tokyo";
+$ noms6 = "tarzan";
+$ noms7 = "viande";
+$ noms8 = "poulet";
+$ noms9 = "avion";
+$ noms = array ( "basket" , "ballon" , "joueur" , "tete" , "libreville" , "tokyo" ,"tarzan" , "viande" , "poulet" , "avion");
+
+echo "Les prenoms" . $ noms [ 0 ]. "," . $ noms [ 1 ]. "et" . $ noms [ 2 ]. "." ; . $ noms [ 3 ]. "," . $ noms [ 4 ]. " , " . $ noms [ 5 ]. "." . $ noms [ 6 ]. "," . $ noms [ 7 ]. "," . $ noms [ 8 ]. " "  $ noms [ 9 ].;
+?> 
+
+</body> 
+</html>
