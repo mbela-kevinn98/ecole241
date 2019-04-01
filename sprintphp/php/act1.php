c2521ea52a71f78c9c482433a43ee01dffb9c144
mbela-kevinn <mbalevin@gmail.com>
2019-04-01 10:49:01 +0200

    ajout de php

--- sprintphp/php/act1.php
+++ sprintphp/php/act1.php
@@ -3,19 +3,14 @@
 <html>
 
     <head>
-        <title > Act2 </title>
+        <title > Act1 </title>
         <meta  charset = " UTF-8 " >
     </head>
 
     <body>
-    	<h1>act2</h1>
+    	<h1>act1</h1>
        <? php
-$ lieu  =  " OkaCode " ;
-$ nombre  =  22 ;
-$ nom  =  " Kevinn " ;
-$ profession  =  " Developpeur " ;
-
-echo  " <p> Je suis $ nom , mon metier c'est $ profession et je suis un membre d' $ lieu ou nous somme à $ nombre <p>";
-? >
+          echo "OkaCode";     
+       ? >
     </body>
 </html>
---
+++ sprintphp/php/acte2.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+
+    <head>
+        <title >Act2</title>
+        <meta  charset = " UTF-8">
+    </head>
+
+    <body>
+    	<h1>act2</h1>
+       <? php
+$ lieu = " OkaCode ";
+$ nombre  =  22 ;
+$ nom  =  " Kevinn ";
+$ profession  =  " Developpeur ";
+
+echo " <p> Je suis $ nom , mon metier c'est $ profession et je suis un membre d'$ lieu ou nous somme à $ nombre <p>";
+	? >
+    </body>
+</html>
