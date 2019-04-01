d64fe540bb006b4b2915b6ab5868671f6263e005
mbela-kevinn <mbalevin@gmail.com>
2019-04-01 11:56:18 +0200

    ajout de php3

---
+++ sprintphp/php/act3.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Act 3</title>
+	<meta charset="utf-8">
+</head>
+<body>
+ <h1>Acte3</h1> <br><br>
+ <h2>Le tableau associatif</h2>
+
+<? php
+
+$poidsAlimens = [1000, 1500, 2000, 5000];
+ var_dump($poidsAliments);
+
+  $poidsAliments [
+  	'riz' => 1000,
+    'poulet' => 1500,
+    'legumes' => 2000,
+    'huil' => 5000,
+];
+? >
+ 
+
+</body>
+</html>
