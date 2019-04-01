506c73bd9a898820a883e0da83f2cc70875fb444
mbela-kevinn <mbalevin@gmail.com>
2019-04-01 10:38:33 +0200

    ajout de php2

--- sprintphp/php/act1.php
+++ sprintphp/php/act1.php
@@ -3,14 +3,19 @@
 <html>
 
     <head>
-        <title > Act4 </title>
+        <title > Act2 </title>
         <meta  charset = " UTF-8 " >
     </head>
 
     <body>
-    	<h1>Tableau</h1>
-        <? php
-          echo "okacode";
-        ?> 
+    	<h1>act2</h1>
+       <? php
+$ lieu  =  " OkaCode " ;
+$ nombre  =  22 ;
+$ nom  =  " Kevinn " ;
+$ profession  =  " Developpeur " ;
+
+echo  " <p> Je suis $ nom , mon metier c'est $ profession et je suis un membre d' $ lieu ou nous somme à $ nombre <p>";
+? >
     </body>
 </html>
